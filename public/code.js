1a7691bc796c2dd546f8c55f343ec37d70318cd0
AnTo3777 <an.moutier123@gmail.com>
2022-08-28 16:13:49 +0200

    Update code.js

--- public/code.js
+++ public/code.js
@@ -87,7 +87,7 @@ function clearDisplay() {
     changeCss('#ball').backgroundColor = 'black';
 };
 function openLeaderboardPage() {
-    window.location.href="./leaderboard/index.html"; 
+    window.location.href="https://ball-game-leaderboard.herokuapp.com"; 
 };
 function errorMessageFunction(text) {
     errorMessage.style.color = 'red';
@@ -388,17 +388,20 @@ document.addEventListener('keydown',(e) => {
 });
 
 /******* get my computer id *******/
-var navigator_info = window.navigator;
-var screen_info = window.screen;
-var uid = navigator_info.mimeTypes.length;
-uid += navigator_info.userAgent.replace(/\D+/g, ''); //contains information about the browser name, version and platform.
-uid += navigator_info.plugins.length;
-uid += screen_info.height || '';
-uid += screen_info.width || '';
-uid += screen_info.pixelDepth || '';
+// var navigator_info = window.navigator;
+// var screen_info = window.screen;
+// var uid = navigator_info.mimeTypes.length;
+// uid += navigator_info.userAgent.replace(/\D+/g, ''); //contains information about the browser name, version and platform.
+// uid += navigator_info.plugins.length;
+// uid += screen_info.height || '';
+// uid += screen_info.width || '';
+// uid += screen_info.pixelDepth || '';
+const uid = 'GIDIGE763839GE8333'
 setTimeout(() => {
     socket.emit('test-id', uid);
 },200);
+
+
 
 
 /******* player movement listener *******/
