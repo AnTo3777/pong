746f9e535a1661720137ff2b55997b8bad1aa13d
AnTo3777 <an.moutier123@gmail.com>
2022-08-28 16:22:33 +0200

    dze

--- public/code.js
+++ public/code.js
@@ -388,15 +388,14 @@ document.addEventListener('keydown',(e) => {
 });
 
 /******* get my computer id *******/
-// var navigator_info = window.navigator;
-// var screen_info = window.screen;
-// var uid = navigator_info.mimeTypes.length;
-// uid += navigator_info.userAgent.replace(/\D+/g, ''); //contains information about the browser name, version and platform.
-// uid += navigator_info.plugins.length;
-// uid += screen_info.height || '';
-// uid += screen_info.width || '';
-// uid += screen_info.pixelDepth || '';
-const uid = 'GIDIGE763839GE8333'
+var navigator_info = window.navigator;
+var screen_info = window.screen;
+var uid = navigator_info.mimeTypes.length;
+uid += navigator_info.userAgent.replace(/\D+/g, ''); //contains information about the browser name, version and platform.
+uid += navigator_info.plugins.length;
+uid += screen_info.height || '';
+uid += screen_info.width || '';
+uid += screen_info.pixelDepth || '';
 setTimeout(() => {
     socket.emit('test-id', uid);
 },200);
